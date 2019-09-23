12b73d112e82c9f52db39a1ebf15bc6d641687af
Fernando Ruiz <linuxuser07@gmail.com>
2019-09-22 23:39:58 -0500

    Adding a StatefulWidget and setting the State for the widget to be able to see the data reflected and also get the app rebuiled

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -6,17 +6,31 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print('Answer Chosen');
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
-  var questions = [
-  'What is your favorite color?',
-  'What\'s is your favorite animal'
-  ];
+}
+
+class MyAppState extends State<MyApp> { 
+  var questionIndex = 0;
+  
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    
+    print(questionIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+  'What is your favorite color?',
+  'What\'s is your favorite animal'
+  ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -24,7 +38,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The Question!'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer1'),
               //reason to pass the function with no parenthesis, is to pass the pointer. 
