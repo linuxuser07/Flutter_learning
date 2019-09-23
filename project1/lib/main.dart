f60d00b1812a547487e3bd14826ebac78ed9afd8
Fernando Ruiz <linuxuser07@gmail.com>
2019-09-22 23:15:00 -0500

    Adding A list of widgets to show buttons learning about annonimous functions

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer Chosen');
+  }
+  var questions = [
+  'What is your favorite color?',
+  'What\'s is your favorite animal'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,7 +22,24 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text('The Question!'),
+            RaisedButton(
+              child: Text('Answer1'),
+              //reason to pass the function with no parenthesis, is to pass the pointer. 
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: () => print('Something else!'),
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: () { print('Option 3');},
+            ),
+          ],
+        ),
       ),
     );
   }
