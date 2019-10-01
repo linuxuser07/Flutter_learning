46a6a1f0c2bc1b3fb7ca5ada4b627bf757d0c370
Fernando Ruiz <linuxuser07@gmail.com>
2019-09-30 19:50:37 -0500

    Added a Adaptive Widget for IOS and Android.

--- expenses_app/lib/widgets/new_transaction.dart
+++ expenses_app/lib/widgets/new_transaction.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:expenses_app/widgets/adaptiveFlatButton.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -52,7 +53,7 @@ class _NewTransactionState extends State<NewTransaction> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-          child: Card(
+      child: Card(
         elevation: 10,
         child: Container(
           padding: EdgeInsets.only(
@@ -92,20 +93,7 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
-                    Platform.isIOS ? CupertinoButton(
-                      child: Text(
-                        'Choose Date',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      onPressed: _presentDatePicker,
-                    )  : FlatButton(
-                      textColor: Theme.of(context).primaryColor,
-                      child: Text(
-                        'Choose Date',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      onPressed: _presentDatePicker,
-                    )
+                    AdaptiveFlatButton('Choose Date', _presentDatePicker),
                   ],
                 ),
               ),
